c06dd5982afd0149479dbbced7f1b4a92fa6c355
Gokul <gokulchandan18@gmail.com>
2021-12-12 16:50:06 +0530

    commit

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 
 export class Shortenpipe implements PipeTransform{
-transform(value: any) {
-    return value.substr(0,10);
+transform(value: any,limit:number) {
+    if(value.length>limit){
+   return value.substr(0,limit)+' ...';
     
+}
+return value;
 }
 }
